f370d42f65a7ece3ce0e00cda6d6deae421e7bb1
Ivan Sosnin <isosnin@skbkontur.ru>
2016-07-11 17:59:53 +0500

    adds plugin implementation

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
+'use strict';
+
 var postcss = require('postcss');
 
-module.exports = postcss.plugin('postcss-omit-import-tilde', function (opts) {
-    opts = opts || {};
-
-    // Work with options here
-
-    return function (css, result) {
-
-        // Transform CSS AST here
-
+module.exports = postcss.plugin('postcss-omit-import-tilde', function () {
+    return function (css) {
+        css.walkAtRules('import', function (rule) {
+            if (rule.params.indexOf('~') !== -1) {
+                rule.params = rule.params.replace('~', '');
+            }
+        });
     };
 });
